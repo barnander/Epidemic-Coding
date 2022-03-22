c582cbd3d9a1ef61ec9ae30867ec564bb79efb5a
Seb Stanbridge <rr21338@it075735.wks.bris.ac.uk>
2022-03-22 14:29:54 +0000

    preliminary plots file

---
+++ plot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 13:05:33 2022
+
+@author: rr21338
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def grid_count(grid,grid_count_list,state):
+    grid_count=len(grid[grid==state])
+    grid_count_list.append(grid_count)
+    return grid_count_list
+
+def plot_show(list_of_infections):
+    x=np.arange(len(list_of_infections))
+    y=np.array(list_of_infections)
+    plt.xlabel('Days(D)')
+    plt.ylabel('Number of infected (S)')
+    plt.plot(x,y) 
+    plt.title("Matplotlib Plot NumPy Array")
+    return plt.show()
+               
+def original_grid(n):
+    row = ["S" for i in range(n)]
+    grid = np.array([row for i in range(n)])
+    return grid
+grid=original_grid(5)
+grid[2,3]='I'
+thing=grid_count(grid,[20,14,8,6],'I')
+plot_show(thing)
+
