10c153dcff21ec00b6284074c930c098b7580fa5
sstanbridge09 <rr21338@bristol.ac.uk>
2022-03-25 14:29:19 +0000

    both factors graphed

--- plot.py
+++ plot.py
@@ -9,17 +9,22 @@ Created on Tue Mar 22 13:05:33 2022
 import numpy as np
 import matplotlib.pyplot as plt
 
-def grid_count(grid,grid_count_list,state):
+def grid_count(grid,grid_count_list,susceptible_list,state,starting_population):
     grid_count=len(grid[grid==state])
+    if state=='I':
+        susceptible=starting_population-grid_count
     grid_count_list.append(grid_count)
-    return grid_count_list
+    susceptible_list.append(susceptible)
+    return grid_count_list,susceptible_list
 
 def plot_show(list_of_infections):
-    x=np.arange(len(list_of_infections))
-    y=np.array(list_of_infections)
-    plt.xlabel('Days(D)')
-    plt.ylabel('Number of infected (S)')
-    plt.plot(x,y,label='Number Infected') 
+    x=np.arange(len(list_of_infections[0]))
+    y=np.array(list_of_infections[0])
+    z=np.array(list_of_infections[1])
+    plt.xlabel('Day(D)')
+    plt.ylabel('Number of People')
+    plt.plot(x,y,label='Number of Infected') 
+    plt.plot(x,z,label='Number of Susceptible')
     plt.title("Matplotlib Plot NumPy Array")
     plt.legend()
     return plt.show()
@@ -28,8 +33,12 @@ def original_grid(n):
     row = ["S" for i in range(n)]
     grid = np.array([row for i in range(n)])
     return grid
-grid=original_grid(5)
+starting_population=100000
+grid=original_grid(100)
 grid[2,3]='I'
-thing=grid_count(grid,[20,14,8,6],'I')
+previous_infected=[20*np.exp(x) for x in range(0,9)]
+previous_susceptible=[100000-x for x in previous_infected]
+thing=grid_count(grid,previous_infected,previous_susceptible,'I',starting_population)
+# print(thing[0])
 plot_show(thing)
 
