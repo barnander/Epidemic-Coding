18f5b2cddcc21b1450af67fa095d5a128a02dbbd
Basil Arnander <nk21253@bristol.ac.uk>
2022-03-29 13:12:18 +0100

    Original Grid + Distance Func

--- grid.py
+++ grid.py
@@ -7,8 +7,21 @@ Created on Tue Mar 22 13:11:22 2022
 
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 def original_grid(n):
     row = ["S" for i in range(n)]
     grid = np.array([row for i in range(n)])
+    i = random.randint(0,n-1)
+    j = random.randint(0, n-1)
+    grid[i,j] = 'I'
     return grid
+
+def in_range(square, radius):
+    affected_squares = []
+    x = square[0]
+    y = square[1]
+    affected_squares = [[x+i,y+j] for i in range(-radius,radius+1) for j in range(-radius,radius+1)]
+    affected_squares.remove(square)
+    return affected_squares
+
