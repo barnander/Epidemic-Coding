8dd3023c54be75d028ecea543ca6eb5b3dd2fe10
FMiners <fjminers@hotmail.co.uk>
2022-05-05 15:07:42 +0100

    altered command lines to become a function in plot to allow for plot saving and usage of args in simulation

--- plot.py
+++ plot.py
@@ -11,9 +11,40 @@ import pandas as pd
 import argparse
 
 parser=argparse.ArgumentParser(description='How each value changes the sim')
-parser.add_argument('--File',metavar='n',type=str,default=None,
-                    help='Give a name for the plot and save it instead of displaying')
-args=parser.parse_args()
+def arg(parser):
+    
+    parser.add_argument('--Size',metavar='N',type=int,default=30,
+                            help='Use a grid of size N x N')
+    parser.add_argument('--Start',metavar='N',type=int,default=3,
+                        help='The number of initial infected at the start of the sim ')
+    parser.add_argument('--Inf',metavar='p',type=float,default=0.3,
+                            help='Chance of infection each day when in range of an infected individual ')
+    parser.add_argument('--Range',metavar='N',type=int,default=2,
+                            help='How far the virus can jump from person to person within the grid')
+    parser.add_argument('--Rec',metavar='p',type=float,default=0.3,
+                            help='Chance to recover each day you are infected ')
+    parser.add_argument('--Death',metavar='p',type=float,default=0.005,
+                            help='Chance of an infetced individual to die each day')
+    parser.add_argument('--Hosprate',metavar='p',type=float,default=0.1,
+                        help='Chance for an infected individual to be hospitalised')
+    parser.add_argument('--Hospcap',metavar='%',type=float,default=0.3,
+                        help='percentage of total population that can be hospitalised before capacity is reached')
+    parser.add_argument('--Demo',metavar='d',default='S',choices=['S','C','E'],
+                        help='Choose what population demographic is simulated, stationary"s", constrictive"c", expansive"e')
+    parser.add_argument('--Vac',metavar='p',type=float,default=0.5,
+                        help='The total proportion of the population that recieves a vaccine')
+    parser.add_argument('--Proc',metavar='p',type=float,default=1.5,
+                        help='Set the factor at which the chance of infection is divided by after recieving the vaccine, must be greater than 1')
+    parser.add_argument('--Immune',metavar='p',type=int,default=10000,
+                        help='the ammount of days that a person remains immune from infection after recovery, after thsi period the become susceptible again')
+    parser.add_argument('--Duration',metavar='T',type=int,default=50,
+                        help='set the duration of the sim to time T')
+    parser.add_argument('--File',metavar='n',type=str,default=None,
+                        help='give a name for the file to be saved under instead of displaying')
+    
+    
+    return parser.parse_args()
+
 
 def grid_count_list(grid_list):
     grid_count_inf_list=grid_count(1,grid_list)
@@ -29,7 +60,7 @@ def grid_count(state,grid_list):
         grid_count_state=len(grid[grid==state])
         grid_count_list.append(grid_count_state)
     return grid_count_list
-
+args=arg(parser)
 def plot_show(grid_list):
     list_of_infections = grid_count_list(grid_list)
     x=np.arange(len(list_of_infections[0]))
--- simulation.py
+++ simulation.py
@@ -7,37 +7,10 @@ from grid import main
 from plot import plot_show
 from animation import grid_animation
 import argparse
+from plot import arg
 
 parser=argparse.ArgumentParser(description='How each value changes the sim')
-parser.add_argument('--Size',metavar='N',type=int,default=30,
-                        help='Use a grid of size N x N')
-parser.add_argument('--Start',metavar='N',type=int,default=3,
-                    help='The number of initial infected at the start of the sim ')
-parser.add_argument('--Inf',metavar='p',type=float,default=0.3,
-                        help='Chance of infection each day when in range of an infected individual ')
-parser.add_argument('--Range',metavar='N',type=int,default=2,
-                        help='How far the virus can jump from person to person within the grid')
-parser.add_argument('--Rec',metavar='p',type=float,default=0.3,
-                        help='Chance to recover each day you are infected ')
-parser.add_argument('--Death',metavar='p',type=float,default=0.005,
-                        help='Chance of an infetced individual to die each day')
-parser.add_argument('--Hosprate',metavar='p',type=float,default=0.1,
-                    help='Chance for an infected individual to be hospitalised')
-parser.add_argument('--Hospcap',metavar='%',type=float,default=0.3,
-                    help='percentage of total population that can be hospitalised before capacity is reached')
-parser.add_argument('--Demo',metavar='d',default='S',choices=['S','C','E'],
-                    help='Choose what population demographic is simulated, stationary"s", constrictive"c", expansive"e')
-parser.add_argument('--Vac',metavar='p',type=float,default=0.5,
-                    help='The total proportion of the population that recieves a vaccine')
-parser.add_argument('--Proc',metavar='p',type=float,default=1.5,
-                    help='Set the factor at which the chance of infection is divided by after recieving the vaccine, must be greater than 1')
-parser.add_argument('--Immune',metavar='p',type=int,default=10000,
-                    help='the ammount of days that a person remains immune from infection after recovery, after thsi period the become susceptible again')
-parser.add_argument('--Duration',metavar='T',type=int,default=50,
-                    help='set the duration of the sim to time T')
-
-
-args=parser.parse_args()
+args=arg(parser)
 
 import pandas as pd
 
