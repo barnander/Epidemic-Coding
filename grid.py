c9c6bafd78bc5c316474728d9772d9908d95541a
FMiners <fjminers@hotmail.co.uk>
2022-05-05 13:49:19 +0100

    changed default values

--- grid.py
+++ grid.py
@@ -13,30 +13,30 @@ import matplotlib.colors as colors
 import argparse
 
 parser=argparse.ArgumentParser(description='How each value changes the sim')
-parser.add_argument('--Size',metavar='N',type=int,default=50,
+parser.add_argument('--Size',metavar='N',type=int,default=30,
                         help='Use a grid of size N x N')
 parser.add_argument('--Start',metavar='N',type=int,default=1,
                     help='The number of initial infected at the start of the sim ')
 parser.add_argument('--Inf',metavar='p',type=float,default=0.3,
-                        help='Chance of infection each day when in range of an infected individual ')
+                        help='Chance of infection each day when in range of an infected individual, must be a value between 0 and 1')
 parser.add_argument('--Range',metavar='N',type=int,default=2,
                         help='How far the virus can jump from person to person within the grid')
 parser.add_argument('--Rec',metavar='p',type=float,default=0.3,
                         help='Chance to recover each day you are infected ')
 parser.add_argument('--Death',metavar='p',type=float,default=0.005,
-                        help='Chance of an infetced individual to die each day')
+                        help='Chance of an infetced individual to die each day, must be a value between 0 and 1')
 parser.add_argument('--Hosprate',metavar='p',type=float,default=0.1,
-                    help='Chance for an infected individual to be hospitalised')
+                    help='Chance for an infected individual to be hospitalised, must be a value between 0 and 1')
 parser.add_argument('--Hospcap',metavar='%',type=float,default=0.3,
-                    help='percentage of total population that can be hospitalised before capacity is reached')
+                    help='proportion of total population that can be hospitalised before capacity is reached, must be a vlaue between 0 and 1')
 parser.add_argument('--Demo',metavar='d',default='S',choices=['S','C','E'],
                     help='Choose what population demographic is simulated, stationary"s", constrictive"c", expansive"e')
 parser.add_argument('--Vac',metavar='p',type=float,default=0.5,
-                    help='The total proportion of the population that recieves a vaccine')
+                    help='The total proportion of the population that recieves a vaccine, must be a value between 0 and 1')
 parser.add_argument('--Proc',metavar='p',type=float,default=1.5,
                     help='Set the factor at which the chance of infection is divided by after recieving the vaccine, must be greater than 1')
-parser.add_argument('--Immune',metavar='p',type=int,default=5,
-                    help='the ammount of days that a person remains immune from infection after recovery, after thsi period the become susceptible again')
+parser.add_argument('--Immune',metavar='p',type=int,default=10000,
+                    help='the ammount of days that a person remains immune from infection after recovery, after this period the become susceptible again')
 parser.add_argument('--Duration',metavar='T',type=int,default=50,
                     help='set the duration of the sim to time T')
 
@@ -399,7 +399,7 @@ def plot_show(list_of_infections):
     axs2=plt.plot(x,c, label='Number of Hospitalised', color='c')
     axs2=plt.plot()
     axs2=plt.title("Population Statistics from Simulation")
-    axs2=plt.legend()
+    axs2=plt.legend(loc='center right')
     axs1.axis('off')
     stats=np.zeros((5,4),dtype=int)
     for counter,i in enumerate(list_of_infections):
